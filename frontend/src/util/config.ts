116a924857767df0665706e9e96c7d293367ad05
rahulk67 <rahulkumawat6767@gmail.com>
2025-10-28 13:48:48 +0530

    dfg

--- frontend/src/util/config.ts
+++ frontend/src/util/config.ts
@@ -1,3 +1,3 @@
-export const baseURL: string = "";
+export const baseURL: string = "http://localhost:8000"; // backend ka url
 export const secretKey : string = "";
 export const projectName: string = "";    
